3f766b3a7a991c0e7364c0daba586020a4541b57
Alexander R. Torrijos <artorrijos@gmail.com>
2016-12-03 03:12:44 +0000

    Refactor to test long-running operation no longer blocks

--- handlers.js
+++ handlers.js
@@ -8,7 +8,7 @@ function sleep(ms) {
 function start(res) {
     console.log("Request handler 'start' was called.");
     
-    exec('ls -lah', function(error, stdout, stderr) {
+    exec('find /', { timeout: 10000, maxBuffe: 20000*1024 }, function(error, stdout, stderr) {
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.write(stdout);
         res.end();
